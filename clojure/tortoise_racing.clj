10ce31a8ef53c7b7a32d450a637402106f246427
mishagreh <mishagreh@gmail.com>
2026-05-26 22:08:36 +0300

    tortoise racing kata

--- clojure/tortoise_racing.clj
+++ clojure/tortoise_racing.clj
@@ -8,17 +8,66 @@
 ;; g = t2 * (v2 - v1)
 ;; t2 = g/(v2 - v1)
 
-(def v1 720)
-(def v2 850)
-(def g 70)
+(def v1 80)
+(def v2 91)
+(def g 37)
 
 (defn f [[number]]
   [number (math/floor-div (* 60 g) (last number))])
 
+;; (defn race [v1 v2 g]
+;;   (let [diff (- v2 v1)]
+;;     [(math/floor-div g diff)
+;;      (math/floor-div (* 60 g) diff)]))
+
 (defn race [v1 v2 g]
-  (let [diff (- v2 v1)]
-    [(math/floor-div g diff)
-     (math/floor-div (* 60 g) diff)]))
+  (let [diff (- v2 v1)
+        fir (float (/ g diff))
+        sec (- fir (int fir))
+        third (* 60 sec)
+        fourth (- third (int third))
+        fifth (* 60 fourth)]
+    [(int fir) (int third) (int fifth)]))
+
+(rem 11 3)
+(quot 70 130)
+(rem 70 130)
+(math/floor-mod 70 130)
+(def diff (- v2 v1))
+(float (/ g diff)) ; 3.3636363
+(int (* 3600 (float (/ g diff)))) ; 12109
+(quot (int (* 3600 (float (/ g diff)))) 3600) ; 12109
+(rem (int (* 3600 (float (/ g diff)))) 3600) ; 1309
+(quot (rem (int (* 3600 (float (/ g diff)))) 3600) 60) ; 21
+(rem (rem (int (* 3600 (float (/ g diff)))) 3600) 60) ; 49
+(int (* 3600 (/ g diff))) ; 12109
+(* 3600 (/ g diff)) ; 133200/11
+(quot g diff)
+(rem g diff)
+(quot (* 3600 (/ g diff)))
+
+(quot (int (* 3600 (/ g diff)))) 3600
+(quot (int (* 3600 (/ g diff))) 3600)
+
+(defn helper [number]
+  [(int number)])
+
+(defn race [v1 v2 g]
+  (if (> v1 v2)
+    nil
+    (let [result-in-seconds (quot (* 3600 g) (- v2 v1))
+          mins-and-secs (rem result-in-seconds 3600)]
+      [(quot result-in-seconds 3600)
+       (quot mins-and-secs 60)
+       (rem mins-and-secs 60)])))
+
+(defn race [v1 v2 g]
+  (if (> v1 v2)
+    nil
+    (let [time-in-secs (quot (* 3600 g) (- v2 v1))]
+      [(quot time-in-secs 3600)
+       (quot (rem time-in-secs 3600) 60)
+       (rem (rem time-in-secs 3600) 60)])))
 
 
 (race v1 v2 g)
