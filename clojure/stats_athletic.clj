28c48bce812407a1302dfec525e2beb9dff1526f
mishagreh <mishagreh@gmail.com>
2026-05-28 09:38:51 +0300

    stat athletic kata second draft

--- clojure/stats_athletic.clj
+++ clojure/stats_athletic.clj
@@ -5,21 +5,26 @@
   (:require [clojure.test :refer :all]
             [clojure.string :as str]))
 
-(def stat "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17")
-(defn stat [strg])
+(def stat1 "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17")
 
-(let [st (sort (map (fn [[h m s]] (reduce + (map #(* %1 %2)
-                                              [3600 60 1]
-                                              (map #(Integer/parseInt %) [h m s]))))
-                 (map #(str/split % #"\|") (str/split stat #", "))))
-      r (- (last st) (first st))
-      a (int (/ (reduce + st) (count st)))
-      m (nth st 2)]
-  (str "Range: " r " Average: " a " Median: " m))
+(defn stat [strg]
+  (let [st (sort (map (fn [[h m s]] (reduce + (map #(* %1 %2)
+                                                [3600 60 1]
+                                                (map #(Integer/parseInt %) [h m s]))))
+                   (map #(str/split % #"\|") (str/split strg #", "))))
+        r (convert (- (last st) (first st)))
+        a (convert (int (/ (reduce + st) (count st))))
+        m (convert (nth st 2))]
+    (str "Range: " r " Average: " a " Median: " m)))
 
 (apply * [2 3 4]) ; 24
 ;; (* [2 3 4])
 (map #(* %1 %2) [2 3 4] [3 4 5])
+(defn convert [secs]
+  (str/join "|" (map #(if (> 10 %) (str "0" %) %) [(quot secs 3600)
+                                                   (quot (rem secs 3600) 60)
+                                                   (rem (rem secs 3600) 60)])))
+(convert 3678)
 
 
 (deftest a-test1
